f4fa9d181be15d9c449126f525c049d65b2f2bcd
Florencia Vega <florvega1598@gmail.com>
2023-09-26 19:03:28 -0300

    Commit 26-9
    Corrección de errores

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const navToggle = document.querySelector(".nav-toggle");
-const menu = document.querySelector(".menu");
-
-
+    const navToggle = document.querySelector(".nav-toggle");
+    const menu = document.querySelector(".menu");
 
 navToggle.addEventListener('click', () => {
     menu.classList.toggle("nav-menu_visible");
 });
+
