26c0f5e206b3211ca014b01c0965e87a92a7494f
Florencia Vega <florvega1598@gmail.com>
2023-10-22 20:31:53 -0300

    Act 22/10

---
+++ secciones/navbar/navbar.js
@@ -0,0 +1,6 @@
+const navToggle = document.querySelector(".nav-toggle");
+const menu = document.querySelector(".menu");
+
+navToggle.addEventListener('click', () => {
+menu.classList.toggle("nav-menu_visible");
+});
